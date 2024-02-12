09601622d3460ab1a41c35f4184b05babc434b00
Gustavo Marques <gustavo-markes@hotmail.com>
2024-02-12 11:06:57 -0300

    Finishing the initial setup

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+}
+
+`
+
+export default EstiloGlobal
